0af74d22e300e0b87f455595aaf4e070d9dd29ed
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-08-17 21:49:55 +0300

    Paddle Movements

---
+++ Assets/Scripts/Raqueta2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raqueta2 : MonoBehaviour
+{
+    public float velocidadMovimiento;
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Vertical2");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * velocidadMovimiento;
+    }
+}
